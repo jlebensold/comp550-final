fixing bugs in the averaging algorithm

code/federator.py
@@ -2,9 +2,8 @@ from utils import build_train_loader, build_test_loader
 import numpy as np
 import torch
 
-ROUNDS = 10
-EPOCHS = 2
-CATEGORIES = ['EducationalInstitution', 'Artist', 'Company']
+ROUNDS = 200
+CATEGORIES = ['EducationalInstitution', 'Artist']
 TEST_SET_SIZE_PER_CLASS = 1_000
 WORKER_SET_SIZE_PER_CLASS = 2_000
 
@@ -14,33 +13,35 @@ class Federator:
         self.model_factory = model_factory
         self.optimizer_factory = optimizer_factory
 
+        # federator keeps a copy of the model lying around
+        # but does no training
+        self.model = model_factory()
+
     def train_rounds(self):
+        # TODO make sure the training set is not in the test set
         test_set = build_test_loader(test_categories=CATEGORIES, size=TEST_SET_SIZE_PER_CLASS)
         for comm_round in np.arange(0, ROUNDS):
-            print("Round {}".format(comm_round))
-            for epoch in np.arange(0, EPOCHS):
-                for idx, worker in enumerate(self.workers):
-                    worker.train_communication_round(build_train_loader(
-                        train_categories=CATEGORIES,
-                        size=WORKER_SET_SIZE_PER_CLASS),
-                    epoch)
-                    worker.valid_epoch(test_set, epoch)
+            for idx, worker in enumerate(self.workers):
+                worker.train_communication_round(build_train_loader(
+                    train_categories=CATEGORIES,
+                    size=WORKER_SET_SIZE_PER_CLASS),
+                comm_round)
+                worker.valid_comm_round(test_set, comm_round)
 
             new_model = self.average_worker_models()
+
             for worker in self.workers:
                 worker.model = new_model()
                 worker.optimizer = self.optimizer_factory(worker.model)
-
 
     def average_worker_models(self):
         acc = {}
         averaged_weights = {}
         for worker in self.workers:
-            for name, param in worker.model.named_parameters():
-                if param.requires_grad:
-                    if name not in acc.keys():
-                        acc[name] = []
-                    acc[name].append(param.data)
+            for name, param in worker.current_model_weights.items():
+                if name not in acc.keys():
+                    acc[name] = []
+                acc[name].append(param.data)
         for key, weights in acc.items():
             # average the weights across the models
             averaged = torch.stack(weights).mean(0)
@@ -48,7 +49,8 @@ class Federator:
 
 
         def new_model_factory():
-            averaged_model = self.model_factory()
-            averaged_model.update_weights(averaged_weights)
-            return averaged_model
+            with torch.no_grad():
+#                averaged_model = self.model_factory()
+                self.model.update_weights(averaged_weights)
+            return self.model
         return new_model_factory

code/worker.py
@@ -9,12 +9,16 @@ class Worker:
         self.name = name
         self.optimizer = optimizer
         self.model = model
+        self.current_model_weights = {}
 
-    def train_communication_round(self, train_loader, epoch):
+    def train_communication_round(self, train_loader, comm_round):
+        self.store_model_weights()
         total_loss = 0
         total_size = 0
         criterion = nn.CrossEntropyLoss()
+
         self.model.train()
+
         for batch_idx, (data, target) in enumerate(train_loader):
             data, target = Variable(data).cuda(), Variable(target).cuda()
             self.optimizer.zero_grad()
@@ -23,16 +27,31 @@ class Worker:
             loss.backward()
             self.optimizer.step()
             if batch_idx % 10 == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch,
+                print('Train Comm Round: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    comm_round,
                     batch_idx * len(data),
                     len(train_loader.dataset),
                     100. * batch_idx / len(train_loader),
                     loss.item())
                 )
+        self.update_params_delta()
+
+    def store_model_weights(self):
+        for name, param in self.model.named_parameters():
+            # TODO: check if this constraint is required:
+            if param.requires_grad:
+                self.current_model_weights[name] = param.data.detach()
 
 
-    def valid_epoch(self, test_loader, epoch):
+    def update_params_delta(self):
+        """ subtract the current model parameters from the updated model parameters"""
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                if name in self.current_model_weights.keys():
+                   self.current_model_weights[name] =  param.data - self.current_model_weights[name]
+
+
+    def valid_comm_round(self, test_loader, comm_round):
         self.model.eval()
         test_loss = 0
         correct = 0
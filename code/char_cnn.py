fixing averaging algorithm

code/char_cnn.py
@@ -10,29 +10,28 @@ import torch.nn as nn
 import torch
 class CharacterLevelCNN(nn.Module):
 
-    def update_weights(self, averaged_weights):
+    def update_weights(self, weights):
         """ here we initialize the weights from a custom dictionary """
-        print("updating weights")
-        print(self.conv1[0].weight)
-        print(averaged_weights['module.conv1.0.weight'])
-        self.conv1[0].weight += averaged_weights['module.conv1.0.weight']
-        self.conv1[0].bias += averaged_weights['module.conv1.0.bias']
-        self.conv2[0].weight += averaged_weights['module.conv2.0.weight']
-        self.conv2[0].bias += averaged_weights['module.conv2.0.bias']
-        self.conv3[0].weight += averaged_weights['module.conv3.0.weight']
-        self.conv3[0].bias += averaged_weights['module.conv3.0.bias']
-        self.conv4[0].weight += averaged_weights['module.conv4.0.weight']
-        self.conv4[0].bias += averaged_weights['module.conv4.0.bias']
-        self.conv5[0].weight += averaged_weights['module.conv5.0.weight']
-        self.conv5[0].bias += averaged_weights['module.conv5.0.bias']
-        self.conv6[0].weight += averaged_weights['module.conv6.0.weight']
-        self.conv6[0].bias += averaged_weights['module.conv6.0.bias']
-        self.fc1[0].weight += averaged_weights['module.fc1.0.weight']
-        self.fc1[0].bias += averaged_weights['module.fc1.0.bias']
-        self.fc2[0].weight += averaged_weights['module.fc2.0.weight']
-        self.fc2[0].bias += averaged_weights['module.fc2.0.bias']
-        self.fc3.weight += averaged_weights['module.fc3.weight']
-        self.fc3.bias += averaged_weights['module.fc3.bias']
+#        print("updating weights")
+#        print(weights['conv1.0.weight'])
+        self.conv1[0].weight += weights['module.conv1.0.weight']
+        self.conv1[0].bias += weights['module.conv1.0.bias']
+        self.conv2[0].weight += weights['module.conv2.0.weight']
+        self.conv2[0].bias += weights['module.conv2.0.bias']
+        self.conv3[0].weight += weights['module.conv3.0.weight']
+        self.conv3[0].bias += weights['module.conv3.0.bias']
+        self.conv4[0].weight += weights['module.conv4.0.weight']
+        self.conv4[0].bias += weights['module.conv4.0.bias']
+        self.conv5[0].weight += weights['module.conv5.0.weight']
+        self.conv5[0].bias += weights['module.conv5.0.bias']
+        self.conv6[0].weight += weights['module.conv6.0.weight']
+        self.conv6[0].bias += weights['module.conv6.0.bias']
+        self.fc1[0].weight += weights['module.fc1.0.weight']
+        self.fc1[0].bias += weights['module.fc1.0.bias']
+        self.fc2[0].weight += weights['module.fc2.0.weight']
+        self.fc2[0].bias += weights['module.fc2.0.bias']
+        self.fc3.weight += weights['module.fc3.weight']
+        self.fc3.bias += weights['module.fc3.bias']
 
 
     def __init__(self, n_classes=14, input_length=1014, input_dim=68,
@@ -65,7 +64,6 @@ class CharacterLevelCNN(nn.Module):
                 module.weight.data.normal_(mean, std)
 
     def forward(self, input):
-#        input = input.transpose(1, 2)
         output = self.conv1(input)
         output = self.conv2(output)
         output = self.conv3(output)

code/federator.py
@@ -25,33 +25,33 @@ class Federator:
         self.writer = SummaryWriter('../training_logs/{}/{}/{}'.format(time,experiment,"Federator"))
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+        self.test_set, _ = build_test_loader(test_categories=CATEGORIES, size=TEST_SET_SIZE_PER_CLASS)
+
     def train_rounds(self, with_replacement, classes_per_worker, same_initilization):
-        test_set, indexes= build_test_loader(test_categories=CATEGORIES, size=TEST_SET_SIZE_PER_CLASS)
 
         for comm_round in np.arange(0, ROUNDS):
             exclude_ids = []
             for idx, worker in enumerate(self.workers):
-                train_categories = np.random.choice(CATEGORIES, classes_per_worker, replace=False)
-                train_loader, indexes = build_train_loader(train_categories, WORKER_SET_SIZE_PER_CLASS, exclude_ids)
+                train_loader, indexes = build_train_loader(worker.train_categories, WORKER_SET_SIZE_PER_CLASS, exclude_ids)
                 worker.train_communication_round(train_loader, comm_round)
 
 #                torch.save(worker.model.state_dict(), "{}/{}_{}.{}".format(MODEL_DIR,self.experiment,worker.name,comm_round)) 
-                worker.valid_comm_round(test_set, comm_round)
+                worker.valid_comm_round(self.test_set, comm_round)
 
                 if not with_replacement:
                     exclude_ids = np.concatenate([exclude_ids, indexes])
 
-            new_model = self.average_worker_models()
+            averaged_weights  = self.average_worker_models()
 
-            # update the model for the federator 
-            self.model = new_model()
-            self.valid_comm_round(test_set, comm_round)
+            with torch.no_grad():
+                # update the model for the federator 
+                self.model.module.update_weights(averaged_weights)
 
+                federator_weights = self.model.state_dict()
+                self.valid_comm_round(self.test_set, comm_round)
 
-
-            for worker in self.workers:
-                worker.model = new_model()
-                worker.optimizer = self.optimizer_factory(worker.model)
+                for worker in self.workers:
+                    worker.model.load_state_dict(federator_weights)
 
     def valid_comm_round(self, test_loader, comm_round):
         self.model.eval()
@@ -73,7 +73,7 @@ class Federator:
 
         acc = 100. * correct / len(test_loader.dataset)
         self.writer.add_scalar('valid/accuracy', acc, (comm_round * len(test_loader)))
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(test_loss, correct, len(test_loader.dataset), acc))
+        print('\nFederator Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(test_loss, correct, len(test_loader.dataset), acc))
 
     def average_worker_models(self):
         acc = {}
@@ -89,9 +89,4 @@ class Federator:
             averaged_weights[key] = torch.nn.Parameter(averaged)
 
 
-        def new_model_factory():
-            with torch.no_grad():
-                self.model.module.update_weights(averaged_weights)
-#                self.model.update_weights(averaged_weights)
-            return self.model
-        return new_model_factory
+        return averaged_weights

code/main.py
@@ -22,23 +22,18 @@ def perform_federated_training(with_replacement, classes_per_worker, same_initil
 
     def model_factory():
         return torch.nn.DataParallel(CharacterLevelCNN(), [0,1,2,3]).cuda()
-    model1 = model_factory()
-    model2 = model_factory()
-    model3 = model_factory()
+        return CharacterLevelCNN().cuda()
 
-    # lets maintain the same initialization state for all models:
-    if same_initilization:
-        model2.load_state_dict(model1.state_dict())
-        model3.load_state_dict(model1.state_dict())
     experiment= "wr.{}_cpw.{}_init.{}".format(with_replacement, classes_per_worker, same_initilization)
-    workers = [
-            Worker(name="Jane", experiment=experiment, model=model1, optimizer=optimizer_factory(model1)),
-            Worker(name="Sally",  experiment=experiment, model=model2, optimizer=optimizer_factory(model2)),
-            Worker(name="Bob", experiment=experiment,  model=model3, optimizer=optimizer_factory(model3))
-            ]
+    def new_worker(name):
+        return Worker(name=name, train_categories=CATEGORIES, 
+                      experiment=experiment, 
+                      model_factory=model_factory, 
+                      optimizer_factory=optimizer_factory)
 
 
-
+    NUM_WORKERS = 10
+    workers = [new_worker("W{}".format(wid)) for wid in np.arange(0,NUM_WORKERS)]
 
     federator = Federator(workers=workers, 
                          optimizer_factory=optimizer_factory, 

code/utils.py
@@ -8,10 +8,8 @@ from torch.autograd import Variable
 from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 
-# for development, we arbitrarily limit the size of our dataset
-DEFAULT_COUNT_FILTER = 2000
-DEFAULT_CATEGORIES = list(['Company', 'EducationalInstitution', 'Artist'])
-DL_ARGS = {'batch_size': 128, 'shuffle': True, 'num_workers': 1 }
+from constants import *
+DL_ARGS = {'batch_size': 64, 'shuffle': True }
 
 def save_struct(filename, struct):
     file = open(filename,'wb')
@@ -52,16 +50,16 @@ def data_from_pickle():
     categories = list(counts.keys())
     return dbpedia, categories
 
-def build_test_loader(test_categories=DEFAULT_CATEGORIES, size=DEFAULT_COUNT_FILTER):
+def build_test_loader(test_categories, size):
     data, categories = data_from_pickle()
     xs_test, y_test, indexes = build_embedding_set(data['test'], test_categories, size)
-    dataset_test = CharacterDataset(xs_test, y_test, all_labels=categories)
+    dataset_test = CharacterDataset(xs_test, y_test, all_labels=CATEGORIES)
     return torch.utils.data.DataLoader(dataset=dataset_test, **DL_ARGS), indexes
 
-def build_train_loader(train_categories=DEFAULT_CATEGORIES, size=DEFAULT_COUNT_FILTER, exclude_ids=[]):
+def build_train_loader(train_categories, size, exclude_ids=[]):
     data, categories = data_from_pickle()
     xs_train, y_train, indexes = build_embedding_set(data['train'], train_categories, size, exclude_ids)
-    dataset_train = CharacterDataset(xs_train, y_train, all_labels=categories)
+    dataset_train = CharacterDataset(xs_train, y_train, all_labels=CATEGORIES)
     return torch.utils.data.DataLoader(dataset=dataset_train, **DL_ARGS), indexes
 
 

code/worker.py
@@ -10,20 +10,20 @@ from tensorboardX import SummaryWriter
 from datetime import datetime
 
 class Worker:
-    def __init__(self, name, model, optimizer, experiment="Undefined"):
+    def __init__(self, name, model_factory, optimizer_factory, train_categories, experiment="Undefined"):
         self.name = name
-        self.optimizer = optimizer
-        self.model = model
+        self.model = model_factory()
+        self.optimizer = optimizer_factory(self.model)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
         self.current_model_weights = {}
         time = datetime.now().strftime("%I_%M%S_{}".format(experiment))
         self.writer = SummaryWriter('../training_logs/{}/{}'.format(self.name, time))
+        self.train_categories = train_categories
 
     def train_communication_round(self, train_loader, comm_round):
         self.store_model_weights()
-        total_loss = 0
-        total_size = 0
+#        print("store...", self.current_model_weights['conv2.0.weight'])
         criterion = nn.CrossEntropyLoss()
 
         self.model.train()
@@ -36,10 +36,10 @@ class Worker:
             loss = criterion(output, target)
             loss.backward()
             self.optimizer.step()
-            self.writer.add_scalar('train/loss', loss.data.item(), 
-                    iteration_number(comm_round, train_loader, batch_idx))
+            self.writer.add_scalar('train/loss', loss.data.item(),  iteration_number(comm_round, train_loader, batch_idx))
             if batch_idx % 10 == 0:
-                print('Train Comm Round: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                print('{}: Train Comm Round: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    self.name,
                     comm_round,
                     batch_idx * len(data),
                     len(train_loader.dataset),
@@ -50,17 +50,16 @@ class Worker:
 
     def store_model_weights(self):
         for name, param in self.model.named_parameters():
-            # TODO: check if this constraint is required:
-            if param.requires_grad:
-                self.current_model_weights[name] = param.data.detach()
+            self.current_model_weights[name] = param.clone()
 
 
     def update_params_delta(self):
         """ subtract the current model parameters from the updated model parameters"""
-        for name, param in self.model.named_parameters():
-            if param.requires_grad:
+        with torch.no_grad():
+            for name, param in self.model.named_parameters():
                 if name in self.current_model_weights.keys():
-                   self.current_model_weights[name] =  param.data - self.current_model_weights[name]
+                       self.current_model_weights[name] =  param - self.current_model_weights[name]
+#        print("delta...", self.current_model_weights['conv2.0.weight'])
 
 
     def valid_comm_round(self, test_loader, comm_round):
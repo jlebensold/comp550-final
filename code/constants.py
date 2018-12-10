b7cbc36d00d035183f36f304cd9002764a5caf0c
Amir Bawab <amirbawab@gmail.com>
2018-12-10 22:19:11 +0000

    adding constants

---
+++ code/constants.py
@@ -0,0 +1,12 @@
+ROUNDS = 10
+CATEGORIES = ['EducationalInstitution', 'Artist', 'Company', 'MeanOfTransportation'] #, 'OfficeHolder']
+TEST_SET_SIZE_PER_CLASS = 500
+WORKER_SET_SIZE_PER_CLASS = 8_000
+
+
+
+CATEGORIES = ['EducationalInstitution', 'Artist'] 
+TEST_SET_SIZE_PER_CLASS = 100
+WORKER_SET_SIZE_PER_CLASS =900
+
+MODEL_DIR = '../models_trained'
